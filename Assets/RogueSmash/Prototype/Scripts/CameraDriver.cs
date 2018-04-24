b38d89f10a6af041d384b0946acea65a795994d1
Ray <ray@raybarrera.com>
2018-04-23 20:15:46 -0700

    Completed camera and character controls

--- Assets/RogueSmash/Prototype/Scripts/CameraDriver.cs
+++ Assets/RogueSmash/Prototype/Scripts/CameraDriver.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraDriver : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class CameraDriver : MonoBehaviour
+{
+	/// <summary>
+	/// Target is what the camera looks at.
+	/// </summary>
+	[SerializeField] private Transform target;
+	[SerializeField] private float verticalOffset;
+	[SerializeField] private float followOffset;
+	[SerializeField] private float followSpeed = 2.0f;
+	[SerializeField] private bool shouldLookAtTarget;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private Vector3 targetPosition;
+	private Vector3 targetDirection;
+	
+	private void FixedUpdate()
+	{
+		Move();
+		Look();
+	}
+
+	private void Move()
+	{
+		targetPosition = new Vector3(target.transform.position.x, target.transform.position.y + verticalOffset, target.transform.position.z - followOffset);
+		targetDirection = targetPosition - transform.position;
+		transform.position += targetDirection * Time.deltaTime * followSpeed;
+	}
+
+	private void Look()
+	{
+		if (shouldLookAtTarget)
+		{
+			transform.LookAt(target);
+		}
 	}
 }
--- Assets/RogueSmash/Prototype/Scripts/CharacterControls.cs
+++ Assets/RogueSmash/Prototype/Scripts/CharacterControls.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
     /// <summary>
-    /// The actor is the game object in the world
+    /// The actor is the game object in the game world
     /// representing this character/entity.
     /// </summary>
     [SerializeField] private GameObject actor;
+
+    /// <summary>
+    /// Movement speed is linearly multiplied by this value.
+    /// </summary>
+    [SerializeField] private float moveSpeedModifier = 3;
+    
     private Vector3 mousePosition;
     private Vector3 lookDirection;
     
-    public void Update()
+    public void FixedUpdate()
     {
         HandleInput();
     }
@@ -23,9 +27,7 @@ public class CharacterControls : MonoBehaviour
         actor.transform.rotation = lookRotation;
         
         Vector3 moveDirection = GetInput();
-        //Vector3 lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //actor.transform.position += moveDirection * Time.deltaTime;
-        actor.transform.Translate(moveDirection * Time.deltaTime, Space.World);
+        actor.transform.Translate(moveDirection * Time.deltaTime * moveSpeedModifier, Space.World);
     }
 
     private Vector3 GetInput()
@@ -41,7 +43,6 @@ public class CharacterControls : MonoBehaviour
         Vector2 mousePos = Input.mousePosition;
         Vector2 relativeMousePos = new Vector2(mousePos.x - Screen.width/2, mousePos.y - Screen.height/2);
         float angle = Mathf.Atan2(relativeMousePos.y, relativeMousePos.x) * Mathf.Rad2Deg * -1;
-        //Debug.Log(String.Format("mouse pos: {0} | center: {1} | angle: {2} ", mousePos, 0.ToString(), angle));
         Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
         return rot;
     }
